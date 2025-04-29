e9652cb036d51668cd163901dd274cabdf1edeec
Mariano Tucat (UNS) <mt@cs.uns.edu.ar>
2025-04-29 02:28:58 -0300

    is this?

--- backend/app.ts
+++ backend/app.ts
@@ -13,11 +13,11 @@ app.use('/books', bookRouter)
 
 // In production, serve static files from the frontend build directory
 // if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+  app.use(express.static(path.join(__dirname, '../frontend/dist2')))
   
   // For all other routes, serve the index.html
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
+    res.sendFile(path.join(__dirname, '../frontend/dist2/index.html'))
   })
 // }
 
